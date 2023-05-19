24cc7b68af86a22c77894ace7061f44f46089dc7
isagreco <isa.greco@vodafone.com>
2023-05-19 14:32:56 +0100

    New stuff

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,28 +1,39 @@
 import React from 'react';
 import './styles.css';
-import profileImage from './profile.jpg';
+import profileImage from './profile.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTiktok, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const About = () => {
-    return (
-      <div className="about-container">
-        <div className="title-container">
-          <h2 className="about-title">About Me</h2>
+  return (
+    <div className="about-container">
+      <div className="title-container">
+        <h2 className="about-title">About Me</h2>
+      </div>
+      <div className="about-content">
+        <div className="image-container">
+          <img className="profile-image" src={profileImage} alt="Profile" />
         </div>
-        <div className="about-content">
-          <div className="image-container">
-            <img className="profile-image" src={profileImage} alt="Profile" />
+        <div className='text-container'>
+          <p>Hey there, awesome people! Looking to dive into the world of tech? Well, you're in luck because I'm here to guide you on this exciting journey. As a self-taught developer and immigrant in the UK, I know firsthand the struggles of not knowing where to begin. But fear not, I'm here to provide the support and resources you need to kickstart your tech dreams.</p>
+          <p>Let's connect and grow together on my TikTok, LinkedIn, and Instagram profiles. Join my community where I share valuable insights, tips, and tricks to help you navigate the tech landscape with confidence. Together, we'll break down barriers, embrace opportunities, and create a thriving network of aspiring tech enthusiasts. Don't miss out on this incredible adventure – let's embark on it together!</p>
+          <div className="social-media-container">
+            <a href="https://www.tiktok.com/@isabellagrecodev" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faTiktok} size="3x" />
+            </a>
+            <span className="icon-space"></span>
+            <a href="https://www.linkedin.com/in/isabella-greco-01960916a/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} size="3x" />
+            </a>
+            <span className="icon-space"></span>
+            <a href="https://www.instagram.com/isabellasgreco" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagram} size="3x" />
+            </a>
           </div>
-          <div className='text-container'>
-          <p>Hey there! I'm Isabella Greco, a millennial Software Engineer rocking it at Vodafone. I'm all about helping fellow tech enthusiasts make their mark in the industry and shape the future of technology.</p>
-          {/* <p>As a frontend developer, I get a kick out of crafting sleek interfaces and diving into software development. But it doesn't stop there! I'm on a mission to inspire and empower the next generation, especially women, to thrive in the tech world. You'll often find me championing the #womenintech movement and actively promoting diversity and inclusivity.</p> */}
-          {/* <p>But it's not just about the work. I'm a firm believer in work-life balance and well-being at work. Taking care of ourselves is crucial, and I'm all for creating a supportive and inclusive environment that fosters personal growth and happiness. #wellbeingatwork is a topic I'm truly passionate about.</p> */}
-          <p>Speaking of passion, I love sharing my insights and experiences at conferences and events. If you're interested in learning about tech careers, frontend development, software development, or just geeking out about the latest trends, let's connect and collaborate!</p>
-       <p>Together, let's build a future where technology thrives, diversity reigns, and opportunities are abundant.</p>
-        </div>
         </div>
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
 
 export default About;
--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles.css';
+import Image from './banner.webp'
 
 const Banner = () => {
   return (
@@ -7,38 +8,33 @@ const Banner = () => {
       <div className="banner__each">
         <div className="banner__each__content-wrapper bannerMask">
           <div className='wrapper'>
-            {/* Banner Image Start */}
             <div className="banner__image">
               <img
-                src="https://makers.tech/wp-content/uploads/2022/01/B2C-LP-Hero-scaled-1.jpg"
+                src={Image}
                 width="1920"
                 alt="Hero Banner"
               />
                 <div className="banner__gradient"></div>
             </div>
-            {/* Banner Image End */}
             <div className="banner__content">
-              {/* Banner Title Start */}
               <h1 className="banner__title aos-init aos-animate" data-aos="fade-down" data-aos-delay="200">
-                Change your career. Transform your life.
+              I help people to get into tech
               </h1>
-              {/* Banner Title End */}
-              {/* Banner Description */}
               <div className="aos-init aos-animate" data-aos="fade-down" data-aos-delay="400">
                 <div className="banner__description">
-                  <p>Unlock your potential and become a sought-after software engineer.</p>
+                  <p>Ready to level up your life? Unlock the digital realm and let's code your way to success together!</p>
                 </div>
                 <div className="btn-wrapper row gx-3">
                   <div className="col-auto mb-3">
-                    <a className="btn" href="#next__section">
-                      Learn More
+                    <a className="btn" href="https://www.tiktok.com/@isabellagrecodev">
+                      Follow me on TikTok
                     </a>
                   </div>
-                  <div className="col-auto mb-3">
+                  {/* <div className="col-auto mb-3">
                     <a className="btn" href="https://www.eventbrite.co.uk/e/learn-to-code-at-makers-information-session-tickets-368212241727">
                       Join our live Q&amp;A session
                     </a>
-                  </div>
+                  </div> */}
                 </div>
               </div>
               {/* Banner Description End*/}
